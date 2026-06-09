d96a47a3350056f7639a9eb7bb4598ad3b2ed7ab
LaharB <laharbaruah96@gmail.com>
2026-06-09 22:08:32 +0530

    Files Updated

--- uart_controller/rtl/uart_top.sv
+++ uart_controller/rtl/uart_top.sv
@@ -26,13 +26,32 @@ module uart_top(
         .rx_clk(rx_clk) 
     );
 
-    
+    uart_tx tx_dut(
+        .tx_clk(tx_clk), 
+        .tx_start(tx_start),
+        .rst(rst),
+        .tx_data(tx_data),
+        .length(length),
+        .parity_type(parity_type), 
+        .parity_en(parity_en),  
+        .stop2(stop2), 
+        .tx(tx_to_rx), 
+        .tx_done(tx_done), 
+        .tx_err(tx_err)
+    );
 
-
-
-
-
-
-
+    uart_rx rx_dut(
+        .rx_clk(rx_clk), 
+        .rx_start(rx_start),
+        .rst(rst), 
+        .rx(tx_to_rx), 
+        .length(length),
+        .parity_type(parity_type), 
+        .parity_en(parity_en),
+        .stop2(stop2),
+        .rx_out(rx_out),
+        .rx_done(rx_done), 
+        .rx_error(rx_err)
+    );
 
 endmodule
