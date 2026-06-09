7bcc00f30c752b012158a0b9237a2c00778ba322
LaharB <laharbaruah96@gmail.com>
2026-06-09 22:12:26 +0530

    Files Updated

--- uart_controller/rtl/uart_top.sv
+++ uart_controller/rtl/uart_top.sv
@@ -59,3 +59,18 @@ module uart_top(
 endmodule
 
 ///////////////////////////////////////////////////////////////////////////
+//interface 
+interface uart_if;
+
+    logic clk, rst;
+    logic tx_start, rx_start;
+    logic [7:0] tx_data;
+    logic [16:0] baud;
+    logic [3:0] length;
+    logic parity_type, parity_en;
+    logic stop2;
+    logic tx_done, rx_done;
+    logic tx_err, rx_err;
+    logic [7:0] rx_out;
+
+endinterface
