140718e57bb85bc3dc7873a99e4351517cc76144
LaharB <laharbaruah96@gmail.com>
2026-06-09 21:31:04 +0530

    Files Updated

--- uart_controller/rtl/uart_rx.sv
+++ uart_controller/rtl/uart_rx.sv
@@ -8,7 +8,7 @@ module uart_rx (
     output reg [7:0] rx_out,
     ouptut logic rx_done, rx_error
 );
-    logic parity = 0;
+    logic parity = 0; //to calc parity from data received
     logic [7:0] datard = 0; //to store data from tx bit by bit 
     int tick_count = 0; //to count the clk tick 
     int bit_count = 0; //to count bit no in datard
@@ -69,14 +69,143 @@ module uart_rx (
                                 case(length)
                                     5: rx_out = datard[7:3];
                                     6: rx_out = datard[7:2];
-                                    
+                                    7: rx_out = datard[7:1];
+                                    8: rx_out = datard[7:0];
                                 endcase
-                            end 
+                            ////////////////////////////////////////////////////
+                            //parity_generator from datard received 
+                                if(parity_type)
+                                    parity = ^datard;
+                                else 
+                                    parity = ~^datard;
+                            end
+
+                        else 
+                            next_state = recv_data; //if tick_count != 7 or 15 & bit_length != 15, stay here 
                     end
+            ////////////////////////////////////////////////////////////////////////////////////////////////
 
+                check_parity : 
+                    begin
+                        if(tick_count == 7)
+                            begin
+                                if(rx == parity) //compare bit received 
+                                    rx_error = 1'b0; 
+                                else    
+                                    rx_error = 1'b1;
+                            end 
+                        else if (tick_count == 15)
+                            begin
+                                next_state = check_first_stop;
+                            end
+                        else 
+                            begin
+                                next_state = check_parity;
+                            end                       
+                    end
+            ///////////////////////////////////////////////////////////////////////////////////////////////
 
+                check_first_stop : 
+                        begin
+                            if(ticK_count == 7)
+                                begin
+                                    if(rx != 1'b1) //if stop_bit != 1 then error 
+                                        rx_error = 1'b1;
+                                    else 
+                                        rx_error = 1'b0;
+                                end    
+                            else if(tick_count == 15)
+                                begin
+                                    if(stop2)
+                                        next_state = check_sec_stop;
+                                    else 
+                                        next_state = done; 
+                                end
+                        end
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+                check_sec_stop :
+                    begin
+                        if(tick_count == 7)
+                            begin
+                                if(rx != 1'b1) //if 2nd stop_bit != 1 then error 
+                                    rx_error = 1'b1;
+                                else 
+                                    rx_error = 1'b0;
+                            end    
+                        else if(tick_count == 15)
+                            begin
+                                next_state = done; 
+                            end
+                        
+                        end
+            ////////////////////////////////////////////////////////////////////////////////////////////
+
+                done :
+                    begin
+                        rx_done = 1'b1;
+                        next_state = idle;
+                        rx_error = 1'b0;
+                    end
+            /////////////////////////////////////////////////////////////////////////////////////////////
+                default : next_state = idle;
+            
             endcase
         end
-    
-
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ ////// tick_count and bit_count 
+    always@(posedge rx_clk)
+        begin
+            case(state)
+                idle :
+                    begin
+                        tick_count <= 0;
+                        bit_count  <= 0;
+                    end
+        ////////////////////////////////////////////////////////////////////////
+                start_bit :
+                    begin
+                        if(tick_count < 15)
+                            tick_count <= tick_count + 1;
+                        else 
+                            tick_count <= 0;
+                    end
+        ///////////////////////////////////////////////////////////////////////////
+                recv_data :
+                    begin
+                        if(tick_count < 15)    
+                            tick_count <= tick_count + 1;
+                        else 
+                            tick_count <= 0; 
+                            //bit_count will incr only in recv_data state  
+                            bit_count <= bit_count + 1; //if count > 15 then go to next bit_position     
+                    end
+        ////////////////////////////////////////////////////////////////////////////
+                check_parity :
+                    if(tick_count < 15)
+                        tick_count <= tick_count + 1;
+                    else
+                        tick_count <= 0;
+        ////////////////////////////////////////////////////////////////////////////
+                check_first_stop :
+                    begin
+                        if(tick_count < 15)
+                            tick_count <= tick_count + 1;
+                        else 
+                            tick_count <= 0;    
+                    end
+        ////////////////////////////////////////////////////////////////////////////
+                check_sec_stop :
+                    begin
+                        if(tick_count < 15)
+                            tick_count <= tick_count + 1;
+                        else 
+                            tick_count <= 0;    
+                    end
+        ////////////////////////////////////////////////////////////////////////////
+                check_first_stop :
+                    tick_count <= 0; 
+                    bit_count  <= 0; 
+            endcase    
+        end
 endmodule
